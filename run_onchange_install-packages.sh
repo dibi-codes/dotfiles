b4bc435147ee63903fbf5d34f313947d38abef71
Daniel Strufe <db@fino.digital>
2024-11-13 13:54:37 +0100

    Setup scripts

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -9,10 +9,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k || echo "powerlevel10k might already be installed"
 
 # Install auto-suggestions
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions  || echo "zsh-autosuggestions might already be installed"
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions  || echo "zsh-autosuggestions might already be installed"
 
 # Install syntax-highlights
-git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting || echo "zsh-syntax-highlighting might already be installed"
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting || echo "zsh-syntax-highlighting might already be installed"
 
 # Install oh-my-tmux
-git clone --depth=1 https://github.com/gpakosz/.tmux.git "${HOME}/.tmux" || echo "oh-my-tmux already installed"
+git clone --depth=1 https://github.com/gpakosz/.tmux.git "${HOME}/.tmux" || echo "oh-my-tmux might already be installed"
