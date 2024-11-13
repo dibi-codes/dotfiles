e31e9a8f32af60c50a5d7c67bdc2e1d87add7609
Daniel Strufe <db@fino.digital>
2024-11-13 13:17:22 +0100

    Setup manually installed packages

---
+++ run_onchange_install-packages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eu
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install p10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k || echo "powerlevel10k might already be installed"
+
+# Install auto-suggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions  || echo "zsh-autosuggestions might already be installed"
+
+# Install syntax-highlights
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting || echo "zsh-syntax-highlighting might already be installed"
+
+# Install oh-my-tmux
+git clone --depth=1 https://github.com/gpakosz/.tmux.git "${HOME}/.tmux" || echo "oh-my-tmux already installed"
