84ba4aff1da002b605545f749a542470ac99b362
Daniel Strufe <db@fino.digital>
2024-11-13 14:57:45 +0100

    Fix omz setup

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -3,7 +3,7 @@
 set -eu
 
 # Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || echo "oh-my-zsh might already be installed"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc --unattended || echo "oh-my-zsh might already be installed"
 
 # Install p10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k || echo "powerlevel10k might already be installed"
@@ -16,3 +16,5 @@ git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git "${
 
 # Install oh-my-tmux
 git clone --depth=1 https://github.com/gpakosz/.tmux.git "${HOME}/.tmux" || echo "oh-my-tmux might already be installed"
+
+chsh -s "$(which zsh)"
